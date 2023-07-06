05ab5af479c9c1a4a34a8a0737110b94c13791dc
Dima <skaizy44@gmail.com>
2023-07-06 17:38:02 +0300

    вів
    іві

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,55 @@
-<script setup></script>
+<script setup>
+import {ref} from 'vue'
+import AppLayout from '../components/AppLayout.vue';
+import {useRootStore} from '@/stores/root'
+import {storeToRefs} from 'pinia'
+
+const rootStore = useRootStore();
+rootStore.getIngredients()
+
+
+const {ingredients} = storeToRefs(rootStore)
+const ingredient = ref(null)
+
+</script>
 
 <template>
-    <h1>home</h1>
+    
+    <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+        <div class="wrapper">
+            <div class="info">
+                <div class="title">Choose your drink</div>
+                <div class="line"></div>
+                <div class="select-wrapper">
+                    <el-select v-model="ingredient" placeholder="Choose your ingredient" size="large" class="select">
+                    <el-option
+                        v-for="item in ingredients"
+                        :key="item.strIngredient1"
+                        :label="item.strIngredient1"
+                        :value="item.strIngredient1"
+                    />
+  </el-select>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
 </template>
 
 <style lang="sass" scoped>
 @import '../assets/styles/main'
+
+.wrapper
+    display: flex
+    align-items: center
+    justify-content: center
+
+.info
+    padding: 80px 0
+    text-align: center
+
+.select-wrapper
+    padding-top: 50px
+
+.select
+    width: 220px
 </style>
